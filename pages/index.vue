86550fe91b6fdcc9ca80200544e8ed47ec76cffe
guerzoni_andrea <guerzoni.andrea91@gmail.com>
2020-10-31 20:20:49 +0100

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
 	<div id="landing-page">
 
 		<div class="heroPanel2 centered with-shadow customBorders1">
-			<img class="background" src="http://assets.guebbit.com/associazioni/images/wallpaper.jpg" alt="">
+			<img class="background" src="https://assets.guebbit.com/associazioni/images/wallpaper.jpg" alt="">
 		    <div class="panel-content">
 		        <div>
 		            <assoc-form/>
